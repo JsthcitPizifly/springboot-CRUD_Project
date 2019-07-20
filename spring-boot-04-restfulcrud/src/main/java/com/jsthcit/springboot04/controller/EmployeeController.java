b7e7dedf5f2c7163a25df98b61318f136bc2db4d
Pizifly <494002013@qq.com>
2019-07-20 09:25:04 +0800

    Add employee

--- spring-boot-04-restfulcrud/src/main/java/com/jsthcit/springboot04/controller/EmployeeController.java
+++ spring-boot-04-restfulcrud/src/main/java/com/jsthcit/springboot04/controller/EmployeeController.java
@@ -1,11 +1,14 @@
 package com.jsthcit.springboot04.controller;
 
+import com.jsthcit.springboot04.dao.DepartmentDao;
 import com.jsthcit.springboot04.dao.EmployeeDao;
+import com.jsthcit.springboot04.entities.Department;
 import com.jsthcit.springboot04.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.Collection;
 
@@ -15,6 +18,8 @@ public class EmployeeController {
     @Autowired
     EmployeeDao employeeDao;
 
+    @Autowired
+    DepartmentDao departmentDao;
     //查询员工列表
     @GetMapping("/emps")
     public String list(Model model) {
@@ -25,8 +30,19 @@ public class EmployeeController {
     }
 
     @GetMapping("/emp")
-    public String toAddPage() {
-
+    public String toAddPage(Model model) {
+        //来到添加页面，查出所有部门在页面显示
+        Collection<Department> departments = departmentDao.getDepartments();
+        model.addAttribute("depts", departments);
         return "emp/add";
+    }
+
+    @PostMapping("/emp")
+    public String addEmp(Employee employee) {
+
+        System.out.println("ssss:" + employee);
+
+        //回到员工列表页面
+        return "redirect:/emps";
     }
 }
